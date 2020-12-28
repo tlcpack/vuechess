5352a277b9c6688d8140b68f0f931001c1f7e028
Taylor Cooke <tcooke@relias.com>
2020-12-28 16:16:55 -0500

    basic chess working

---
+++ chess-game/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <chessboard />
+  </div>
+</template>
+
+<script>
+import { chessboard } from "vue-chessboard";
+import "vue-chessboard/dist/vue-chessboard.css";
+
+export default {
+  name: "app",
+  components: {
+    chessboard,
+  },
+};
+</script>
